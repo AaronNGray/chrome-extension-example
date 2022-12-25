7c68c8538973456689cfcedfd3adc8b9b0e527f1
AaronNGray <aaronngray@gmail.com>
2022-12-25 15:53:33 +0000

    renamed extension to chrome-extension-example

--- devtools.js
+++ devtools.js
@@ -1,1 +1,1 @@
-chrome.devtools.panels.create("Dev Tools from chrome-extension-boilerplate-react","icon-34.png","panel.html");
+chrome.devtools.panels.create("Dev Tools from chrome-extension-example","icon-34.png","panel.html");
